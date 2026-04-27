b1f8295f42e8917939efe1f981d42ded28c60a6e
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-04-27 10:49:23 -0700

    added comments to day 1

--- Day_1.js
+++ Day_1.js
@@ -1,6 +1,19 @@
 // get filestream module
 const fs = require('fs');
-const { report } = require('process');
+
+/* 
+ * To solve this puzzle, I first stored the expense report in a file input
+ * to read from. I converted the buffer data into a string (str) and then split the string
+ * based on the delimiter "\r\n" which is carriage return & newline. I stored that input
+ * into an array (strArr) and looping through each value, I converted each string into an int
+ * and stored it in another array (reportArr). I sorted the array from greatest to least
+ * and used a nested for loop to find both operands. The first loop holds one number at a time
+ * and determines the second number I need for both to equal 2020. The second loop loops through
+ * the entire array and if it finds the target number, it computes the result and logs it.
+ * 
+ * Cited Sources:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort 
+ */
 
 // read the expense report file
 fs.readFile('expense_report.txt', (err, data) => {
@@ -49,9 +62,13 @@ fs.readFile('expense_report.txt', (err, data) => {
 
         // loop through array and try to find target
         for (i = 0; i < reportArr.length; i++) {
+            // if the target has been found while looping again
             if (reportArr[i] == target) {
+                // hold the result
                 op2 = reportArr[i];
+                // multiply together
                 result = op1 * op2;
+                // log result to console
                 console.log(result);
                 return;
             }
