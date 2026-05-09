de798308268f4de91b79425acc4c96b2ecddf193
remieClaire <remie-claire-danielle.enriquez.090@my.csun.edu>
2026-05-08 20:07:07 -0700

    added comments to Day 4

--- Day_4.js
+++ Day_4.js
@@ -2,7 +2,20 @@
 const fs = require('fs');
 
 /*
- *
+ * Since this puzzle has two parts, I again split my code into two functions. 
+ * For part 1, I noticed that each entry in the text file was separated by one
+ * giant newline- so, I separated each entry based on that using the split() method
+ * and stored each entry in an array. However, even after doing that, entries 
+ * were split via just an empty space ''. Fortunately, I was able to use this
+ * empty space to my advantage by marking it as a separator between entries. So in my loop,
+ * whenever I hit an empty space, I sent my entire entry into a check function. Using the functions
+ * every() and includes() together, the entry would pass so long as every field was in it. 
+ * If this function returned true, then the total would be incremented. 
+ * 
+ * For part two, I tried implementing the same solution but using regex. Unfortunately, I wasn't
+ * able to recreate it using regex for reasons I couldn't figure out in time. I had to switch out
+ * includes() with test() since a quick google search told me that includes() didn't work with regex
+ * functions. 
  *
  * 
  * Cited Sources:
@@ -138,12 +151,9 @@ function partTwo() {
 function checkContents(entry) {
     const fieldReqs = [/^byr:(19[2-9]\d|200[0-2])$/, /^iyr:(201\d|2020)$/, /^eyr:(202\d|2030)$/, /^hgt:(1([5-8]\d|9[0-3])cm|(59|6\d|7[0-6])in)$/, /^hcl:#[a-f0-9]{6}$/, /^ecl:(amb|blu|brn|gry|grn|hzl|oth)$/, /^pid:[0-9]{9}$/]
 
-    // split entry & check each one individually + remove whitespace
-    const field = entry.split(" ").filter( (i) => i !== '');
-    console.log(field)
     // when used on an array, every() returns false if it finds
     // an element that does not satisfy the testing function
-    return fieldReqs.every ((f) => f.test(field));
+    return fieldReqs.every((f) => f.test(entry));
 }
 
 partTwo()
